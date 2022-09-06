242db861353cccc635a35569ee2450ec338c6c79
hendrix-dai <865058222@qq.com>
2022-09-06 16:25:29 +0800

    fix launch group_rank

--- test_launch.sh
+++ test_launch.sh
@@ -1,1 +1,1 @@
-torchrun --nnodes $1 --nproc_per_node=$2 --node_rank $3 --master_addr="127.0.0.1"  --master_port="29700" tests/test_launch.py
+torchrun --nnodes $1 --nproc_per_node=$2 --node_rank $3 --master_addr="0.0.0.0"  --master_port="29700" tests/test_launch.py
