61ac9af02a7b8a7668d136b75bca8262971ca2d7
hendrix-dai <865058222@qq.com>
2022-10-01 21:57:01 +0800

    add basic agent

--- component/agent.py
+++ component/agent.py
@@ -2,27 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import environment as envs
+from typing import List
 
 class BaseAgent():
 
-    def __init__(self, env_name, algorithm) -> None:
-        self.env_name = env_name
+    def __init__(self, env, algorithm, agent_ids:List[str]) -> None:
         self.algorithm = algorithm
+        self.env = env
+        self.agent_ids = agent_ids
 
-    def _make_env(self):
-        env = envs.load(self.env_name + ".py").Scenario()
-        return env                                                                               
+    def _batch_take_action(self):
+        actions = []
+        for a_id in self.agent_ids:
+            obs = self._get_obs(a_id)
+            actions.append(self.algorithm.take_action(obs))
 
-    def _do_action(self):
+    def _get_reward(self, a_id):
         pass
 
-    def _get_reward(self):
+    def _get_obs(self, a_id):
         pass
 
-    def _get_obs(self):
-        pass
-
-    def _get_comm(self):
+    def _get_comm(self, a_id):
         pass
 
     def save_rollout(self):
