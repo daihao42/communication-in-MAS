17ae06039291fcf9ea5a6e2603b1ecc5a0d30553
hendrix-dai <865058222@qq.com>
2022-10-09 15:47:11 +0800

    change to single parallelzed

--- component/agent.py
+++ component/agent.py
@@ -3,24 +3,47 @@
 
 import environment as envs
 from typing import List
+from threading import RLock
 
-class BaseAgent():
+from utils.distributed import DistributedComm
+from component.envWrapper import ParallelizedEnvWrapper
 
-    def __init__(self, env, algorithm, agent_ids:List[str]) -> None:
-        self.algorithm = algorithm
-        self.env = env
-        self.agent_ids = agent_ids
+class ParallelizedAgent():
+    """
+    these agents take actions follow a same policy
+    while they can be worked in different (parallel) envs 
+    """
+
+    def __init__(self, env_list: List[ParallelizedEnvWrapper], learner_rank: int, dist_comm:DistributedComm) -> None:
+        self.lock =RLock()
+        self.envs = env_list
+        self.agent_ids = []
+        self.learner_rank = learner_rank
+        self.dist_comm = dist_comm
+        self.action_space = envs[0].action_space
+
+    def add_hook(self, env:ParallelizedEnvWrapper, agent_id:str):
+        self.lock.acquire()
+        try:
+            self.envs.append(env)
+            self.agent_ids.append(agent_id)
+        finally:
+            self.lock.release()
 
     def _batch_take_action(self):
-        actions = []
-        for a_id in self.agent_ids:
-            obs = self._get_obs(a_id)
-            actions.append(self.algorithm.take_action(obs))
+        obs_batch = []
+        for env,a_id in zip(self.envs,self.agent_ids):
+            obs_batch.append(self._get_obs(env,a_id))
+        return self._remote_batch_inference(obs_batch)
+
+    def _remote_batch_inference(self, input_tensors):
+        self.dist_comm.write_p2p_message([self.learner_rank], [input_tensors])
+        return self.dist_comm.read_p2p_message(msg_shape=(len(self.agent_ids),self.action_space))
 
     def _get_reward(self, a_id):
         pass
 
-    def _get_obs(self, a_id):
+    def _get_obs(self, env, a_id):
         pass
 
     def _get_comm(self, a_id):
